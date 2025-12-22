fe774ab2dd993839418b4e7c107b7ede9912a87a
8Bitsfun <ericalam@live.ca>
2025-12-21 20:33:49 -0800

    archive sub-navigation overlay interaction experiments

--- js/subNav.js
+++ js/subNav.js
@@ -1,3 +1,4 @@
+
 $('.nav a').on('click', function (e) {
     e.preventDefault(); // remove if you want normal navigation
     $('.nav a').removeClass('active-navLink'); // remove active-navLink from all links
@@ -10,3 +11,5 @@ $('.subNav a').on('click', function (e) {
     $('.subNav a').removeClass('active-subNavLink');
     $(this).addClass('active-subNavLink');
 });
+
+
